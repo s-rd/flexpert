32634238c8fea4e14338b3787d4a7e69765b080a
Simon Dagfinrud <17279882+semonje@users.noreply.github.com>
2019-05-05 04:09:19 +0200

    Add navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,8 @@
 <template>
   <nav class="nav-bar">
     <a href="#" class="nav-bar__logo">Flexpert</a>
+    <h3 class="nav-bar__slogan">Flex is difficult. Search it up.</h3>
+    <a href="#" class="nav-bar__link">Playground</a>
   </nav>
 </template>
 
