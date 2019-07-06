836e7c09d443b0c00f59d8a4e9bc03a4c5bf1712
Simon Dagfinrud <17279882+semonje@users.noreply.github.com>
2019-07-07 01:34:57 +0200

    Output tabs

--- src/components/FlexOutput.vue
+++ src/components/FlexOutput.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="flex-output">
-    <ul class="playground__tab-bar">
-      <li
-        v-for="tab in tabs"
-        class="playground__tab"
-        :class="activeTabId === tab.id && 'playground__tab--active'"
-      >
-        <a href="#" @click="setactivetab(tab.id)">{{ tab.title }}</a>
-      </li>
-    </ul>
-
-    <pre><code>.container {{ input.container }}
+    <tab-group :tabs="tabs" :active-tab.sync="tab">
+      <div v-show="tab === 0">
+        No CSS output yet ¯\_(ツ)_/¯
+      </div>
+      <div v-show="tab === 1">
+        <pre><code>.container {{ input.container }}
 
 .child {{ input.items }}
 
 #child-1 {
      flex: 1;
 }</code>
-    </pre>
+        </pre>
+      </div>
+    </tab-group>
   </div>
 </template>
 
 <script>
+import TabGroup from './TabGroup'
+
 export default {
   name: 'flex-ouput',
   props: {
@@ -30,30 +29,25 @@ export default {
       required: true,
     },
   },
+  components: {
+    TabGroup,
+  },
   data() {
     return {
       tabs: [
         {
-          id: 1,
+          i: 0,
           title: 'CSS Output',
         },
         {
-          id: 2,
+          i: 1,
           title: 'SCSS Output',
         },
       ],
-      activeTabId: 2,
+      tab: 1,
     }
   },
-  methods: {
-    setActiveTab(id) {
-      this.activeTabId = id
-    },
-  },
   computed: {
-    activeTab() {
-      return this.tabs[this.activeTabId]
-    },
     sassOutput() {
       return ''
     },
