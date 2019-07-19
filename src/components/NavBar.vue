b5f257ba16ea358ceea515d27c02cde8ea050162
Simon Dagfinrud <17279882+semonje@users.noreply.github.com>
2019-07-20 01:37:59 +0200

    Move mobile note

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,10 +5,12 @@
     <h3 class="nav-bar__slogan">CSS Flexbox Playground</h3>
     <a href="https://twitter.com/semonje" target="_blank" class="nav-bar__link">@semonje</a>
 
-    <p class="nav-bar__mobile-note">
-      This thing works on virtually any modern device, but it'll
-      probably be more valuable on a device with a larger display.
-    </p>
+    <div class="nav-bar__mobile-note">
+      <p>
+        This thing works on virtually any modern device, but it'll
+        probably be more valuable on a device with a larger display.
+      </p>
+    </div>
 
   </nav>
 </template>
