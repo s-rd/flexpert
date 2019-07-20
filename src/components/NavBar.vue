36a2fb5002e05012363c7dacf41c8dd7908808f4
Simon Dagfinrud <17279882+semonje@users.noreply.github.com>
2019-07-20 02:18:12 +0200

    Add hide mobile note method

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,10 +5,11 @@
     <h3 class="nav-bar__slogan">CSS Flexbox Playground</h3>
     <a href="https://twitter.com/semonje" target="_blank" class="nav-bar__link">@semonje</a>
 
-    <div class="nav-bar__mobile-note">
+    <div class="nav-bar__mobile-note" v-if="showNote">
       <p>
         This thing works on virtually any modern device, but it'll
         probably be more valuable on a device with a larger display.
+        <a href="#" class="nav-bar__mobile-close" @click="closeNote">Sure, whatever</a>
       </p>
     </div>
 
@@ -18,5 +19,27 @@
 <script>
 export default {
   name: 'nav-bar',
+  data() {
+    return {
+      showNote: true,
+    }
+  },
+  mounted() {
+    this.detectShowNote()
+  },
+  methods: {
+    closeNote() {
+      this.showNote = false
+      localStorage.setItem('mobileNote', false)
+    },
+    detectShowNote() {
+      if (localStorage.getItem('mobileNote')) {
+        this.showNote = JSON.parse(localStorage.getItem('mobileNote'))
+      } else {
+        this.showNote = true
+        localStorage.setItem('mobileNote', true)
+      }
+    },
+  },
 }
 </script>
